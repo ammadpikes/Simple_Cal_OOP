a1aae94fb2d923f42347032150f4c9a657127fd7
Ammad Ahmed <ammad.ahmed@pikessoft.com>
2015-10-10 16:14:16 +0500

    Options added - Structured Properly

--- csv_ruby.rb
+++ csv_ruby.rb
@@ -2,9 +2,55 @@
 
 require 'csv'
 
+class CSVReadWrite
 
-CSV.foreach("sample_data.csv") do |row|
+def print_csv
+  read_csv
 
-  print row
+  for row in @data
+    puts row.inspect
+  end
 
 end
+
+  def read_csv 
+    @data = CSV.read("sample_data.csv")
+  end
+
+  private :read_csv
+
+end
+
+
+option = 0 
+
+puts "CSV Handling In Ruby"
+puts "--------------------"
+
+while option!=1 || option!=2 do
+
+  print "\nPlease Enter 1 to Read and 2 To Write CSV : "
+  option = gets.to_i
+
+  if option == 1
+    csv = CSVReadWrite.new
+    csv.print_csv
+    break
+  elsif option == 2
+
+    break
+  else
+    print "Invalid Option!"
+  end
+
+end
+
+
+
+
+
+
+
+
+
+
